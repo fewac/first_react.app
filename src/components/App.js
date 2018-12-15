20ab5951fd60312e0b67357472a76a5ee3e1013c
fewac <hadriel84@hotmail.com>
2018-12-15 14:38:19 -0600

    another commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
+import Example from './Example'
+import Title from './Title'
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      title: 'My first react app something'
+    }
+  }
+
   render() {
     return (
-      <h1>
-        My First React App
-      </h1>
-    );
+      <Title title={ this.state.title } />
+    )
   }
+
+
+
+  // render() {
+  //   return (
+  //     <h1>
+  //       { this.state.title }
+  //     </h1>
+  //   )
+  // }
+
 }
+
+  //   render() {
+//     return (
+//       <Example />
+//     );
+//   }
+// }
 
 export default App;
---
+++ src/components/Example.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Example = () => {
+     return (
+         <h1> My second react component</h1>
+     )
+}
+
+export default Example
