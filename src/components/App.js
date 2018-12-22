425082e0e9e8f812f540a5adeec6919ba2481b8d
fewac <hadriel84@hotmail.com>
2018-12-22 14:12:50 -0600

    To do list creation

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import Example from './Example'
-import Title from './Title'
 
 class App extends Component {
 
@@ -8,13 +6,121 @@ class App extends Component {
     super(props)
 
     this.state = {
-      title: 'My first react app something'
+      title: '',
+      list: []
     }
   }
 
+
+  inputHandler = (e) => {
+    //console.log(e.target.value);
+
+    this.setState({
+      title: e.target.value
+    })
+  }
+
+
+  clickHandler = () => {
+    let title = this.state.title;
+    const list = this.state.list;
+    title = title.trim();
+
+
+
+    if (title && title.length){
+      list.push(title);
+
+      this.setState({
+        title: '',
+        list  // esto es igual que si tuviéramos esto:   list: list
+      })
+      
+    }
+
+    console.log(this.state.list)
+    
+    this.setState({
+      title: ''
+    })
+    
+// Otra forma de hacer es así: 
+
+    // if (title.length !== 0){
+    //   list.push(title);
+    //   this.setState({
+    //     list
+    //   }, () => {
+    //     console.log(this.state.list)
+    //   })
+
+     
+    //   }
+    //   this.setState({
+    //     title: ''
+    //      });
+
+    
+
+    // console.log("This is a test");
+    // let elemento;
+    // elemento = this.state.title;
+    // this.setState.list.push(elemento);
+    // console.log(this.state.list);
+  }
+
+
   render() {
+
+    let list = this.state.list
+    /*list = list.map(item => <li>{item}</li>)*/
+    // la arrow function que está adentro de nuestra función "map", no necesita
+    // paréntesis porque todo está dentro de un solo "div" y lo toma como que sólo
+    // está regresando una sola cosa.
+
+    list = list.map(item =>
+      <div className="card bg-light my-1">
+        <div className="card-body">
+          <h5 className="card-title"> {item}</h5>
+        </div>
+      </div>
+    )
+
+
     return (
-      <Title title={ this.state.title } />
+      <div className="container">
+        <div className="row justify-content-center">
+
+          <div className="col-md-6">
+            <div className="card bg-light">
+              <div className="card-body">
+                <h1 className="card-title text-center"> To Do List</h1>
+                  <div className="input-group mb-3"> 
+                      <input 
+                        className = "form-control" 
+                        type = "text" 
+                        value = {this.state.title}
+                        onChange = { (e) => this.inputHandler(e) }
+                        />
+
+                      {/* this.state.title */}
+
+                      <br />
+
+                      <div className="input-group-append">
+                        <button type="button" className="btn btn-primary" onClick={this.clickHandler}>Hola</button>
+                      </div>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col">
+              {list}
+          </div>
+        </div>
+      </div>
     )
   }
 
--- src/components/Example.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Example = () => {
-     return (
-         <h1> My second react component</h1>
-     )
-}
-
-export default Example
